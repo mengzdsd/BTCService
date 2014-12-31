e713ca75fce32b5be3db8203600f0dcc51090788
Mengz You <mengz.you@gmail.com>
2015-01-01 04:44:37 +0800

    [dev] Impleted the subsrt script funcitons.

--- public_html/server-script/genSubTitle.php
+++ public_html/server-script/genSubTitle.php
@@ -12,8 +12,8 @@ error_reporting(E_ALL);
  */
 function getTimeStamp($strDate,$strTime) {
   $strDateTime = $strDate . ' ' . $strTime;
-  $objDateTime = date_create_from_format('Y-m-d H:i:s',$strDateTime);
-  $intTimeStamp = date_timestamp_get($objDateTime);
+  $objDateTime = DateTime::createFromFormat('Y-m-d H:i:s',$strDateTime);
+  $intTimeStamp = $objDateTime->getTimestamp();
   return $intTimeStamp;
 }
 
@@ -41,10 +41,14 @@ function strTrim($str,$strTrim) {
   }
 }
 
+function secondsToStr($intSeconds) {
+  return gmdate('H:i:s',$intSeconds) . ',000';
+}
+
 $strInputFilePath = "./test.srt";
 $strOutputFilePath = "output.srt";
-$strVideoStartTime = "11:00:05";
-$strVideoEndTime = "11:05:55";
+$strVideoStartTime = "11:00:04";
+$strVideoEndTime = "11:05:56";
 $strVideoDate = "2014-12-30";
 
 $strIFRealPath = realpath($strInputFilePath);
@@ -89,11 +93,111 @@ print_r($arrSrtSections);
 
 $intVideoStartTimeStamp = getTimeStamp($strVideoDate,$strVideoStartTime);
 $intVideoEndTimeStamp = getTimeStamp($strVideoDate,$strVideoEndTime);
-echo $intVideoStartTimeStamp, PHP_EOL;
-echo $intVideoEndTimeStamp, PHP_EOL;
+# echo $intVideoStartTimeStamp, PHP_EOL;
+# echo $intVideoEndTimeStamp, PHP_EOL;
+# 
+# $arrSTs = getTimeStamps($strVideoDate,$arrSrtSections[0][1]);
+# echo $arrSTs[0], PHP_EOL;
+# echo $arrSTs[1], PHP_EOL;
+#
+$intRt = 0;
+$arrSrtDatas = array();
+foreach ($arrSrtSections as $arrSrtSection) {
+  $arrTimes = getTimeStamps($strVideoDate,$arrSrtSection[1]);
+  $arrTemp = array($arrTimes[0],$arrTimes[1],$arrSrtSection[2]);
+  array_push($arrSrtDatas,$arrTemp);
+}
+#print_r($arrSrtDatas);
+$intStartN = 0;
+$intEndN = 0;
+$intCount = count($arrSrtDatas);
+#echo $intCount, PHP_EOL;
+#echo $arrSrtDatas[0][0] . ' - ' . $arrSrtDatas[$intCount-1][1] , PHP_EOL;
+if (($intVideoEndTimeStamp <= $arrSrtDatas[0][0]) || ($intVideoStartTimeStamp >= $arrSrtDatas[$intCount-1][1])) {
+  $errorMessage = <<<EOD
+Error: The video's period is not in the period of this srt file.
+(The start time of video later on this srt file end, or the end time of this video early than tis srt file start.)
 
-$arrSTs = getTimeStamps($strVideoDate,$arrSrtSections[0][1]);
-echo $arrSTs[0], PHP_EOL;
-echo $arrSTs[1], PHP_EOL;
+EOD;
+  die($errorMessage);
+}
+
+for ($i = 0; $i < $intCount; $i++) {
+#  echo $intVideoStartTimeStamp . ' - ' . $arrSrtDatas[$i][1] , PHP_EOL;
+  if ($intVideoStartTimeStamp < $arrSrtDatas[$i][1]) {
+    $intStartN = $i;
+    break;
+  }
+}
+
+for ($i = 0; $i < $intCount; $i++) {
+#  echo $intVideoEndTimeStamp . ' + ' . $arrSrtDatas[$i][0] , PHP_EOL;
+  if ($intVideoEndTimeStamp <= $arrSrtDatas[$i][0]) {
+    $intEndN = $i - 1;
+    break;
+  }
+  $intEndN = $i;
+}
+
+#echo $intStartN . ' : ' . $intEndN , PHP_EOL;
+#echo secondsToStr(1419937556 - 1419937204) , PHP_EOL;
+
+if ($intStartN == $intEndN) {
+  $intElapse = $arrSrtDatas[$intStartN][0] - $intVideoStartTimeStamp;
+  $strStartTime = "";
+  $strEndTime = "";
+  if ($intElapse > 0) {
+    $strStartTime = secondsToStr($intElapse);
+  } else {
+    $strStartTime = '00:00:00,000';
+  }
+
+  $intElapse = $arrSrtDatas[$intEndN][1] - $intVideoEndTimeStamp;
+  if ($intElapse > 0) {
+    $strEndTime = secondsToStr($intVideoEndTimeStamp - $intVideoStartTimeStamp);
+  } else {
+    $strEndTime = secondsToStr($arrSrtDatas[$intEndN][1] - $intVideoStartTimeStamp);
+  }
+
+  $strTimeLine = $strStartTime . ' --> ' . $strEndTime . "\r\n";
+  fputs($rsOF,"0\r\n");
+#  echo $strTimeLine;
+  fputs($rsOF,$strTimeLine);
+#  echo $arrSrtDatas[$intStartN][2] . "\r\n";
+  fputs($rsOF,$arrSrtDatas[$intStartN][2]);
+} else {
+  for ($i = $intStartN; $i <=$intEndN; $i++) {
+    $strStartTime = "";
+    $strEndTime = "";
+    if ($i == $intStartN) {
+      $intElapse = $arrSrtDatas[$i][0] - $intVideoStartTimeStamp;
+      if ($intElapse > 0 ) {
+	$strStartTime = secondsToStr($intElapse);
+      } else {
+	$strStartTime = '00:00:00,000';
+      }
+      $strEndTime = secondsToStr($arrSrtDatas[$i][1] - $intVideoStartTimeStamp); 
+    } elseif ($i == $intEndN) {
+      $intElapse = $arrSrtDatas[$i][1] - $intVideoEndTimeStamp;
+      if ($intElapse > 0 ) {
+	$strEndTime = secondsToStr($intVideoEndTimeStamp - $intVideoStartTimeStamp);
+      } else {
+	$strEndTime = secondsToStr($arrSrtDatas[$i][1] - $intVideoStartTimeStamp);
+      }
+      $strStartTime = secondsToStr($arrSrtDatas[$i][0] - $intVideoStartTimeStamp);
+    } else {
+      $strStartTime = secondsToStr($arrSrtDatas[$i][0] - $intVideoStartTimeStamp);
+      $strEndTime = secondsToStr($arrSrtDatas[$i][1] - $intVideoStartTimeStamp);
+    }
+    $strSeq = ($i - $intStartN) . "\r\n";
+    $strTimeLine = $strStartTime . ' --> ' . $strEndTime . "\r\n";
+    fputs($rsOF,$strSeq);
+#    echo $strTimeLine;
+    fputs($rsOF,$strTimeLine);
+#    echo $arrSrtDatas[$i][2] . "\r\n";
+    fputs($rsOF,$arrSrtDatas[$i][2]);
+    fputs($rsOF,"\r\n\r\n");
+  }
+}
 
 fclose($rsOF);
